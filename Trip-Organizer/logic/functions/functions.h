eda14e8cf0d86f1c1e07ea150cfb5190200b0746
Omar Ntifi Matarín <omar.ntifi@matagalls.salleurl.edu>
2018-12-28 18:33:35 +0000

    Feat: Some headers were missing in file functions.h. Headers have been added

--- Trip-Organizer/logic/functions/functions.h
+++ Trip-Organizer/logic/functions/functions.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include "../../model/list/list.h"
 
+void showAllDestinations(List *l);
+int comparePrices(const void * a, const void * b);
+void planTrip(List *l);
+void readNewFile(List * l);
 void execute(int option, List * l);
 void readWholeFile(FILE * f, List * l);
 
