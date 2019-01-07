aee488da7ca139a8bce66e993a542deafea4ca34
Omar Ntifi Matarín <omar.ntifi@matagalls.salleurl.edu>
2019-01-07 20:55:48 +0000

    Fix: memory released in removePoi function

--- Trip-Organizer/model/list/list.c
+++ Trip-Organizer/model/list/list.c
@@ -102,12 +102,12 @@ void removePoi(List * l, int mode) {
 		Node * aux = l -> poi[mode];
 		int i;
 		//Deslliguem el node de la resta
-		//for (i = 0; i < MAX_SORTING; i++) {
 			l -> poi[i] = aux -> next[i];
 
 			aux -> prev[i] -> next[i] = aux -> next[i];
 			aux -> next[i] -> prev[i] = aux -> prev[i];
-		//}
+		freeAll(aux->dest);
+		free(aux);
 		l -> n--;
 	}
 }
